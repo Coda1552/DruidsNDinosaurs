832b13b105187da28459c3744d6c5ebba064bd6e
Killerkrow <cpf283@gmail.com>
2026-05-20 10:26:42 -0500

    I have no clue how to Port. But I think I got the bare minimum of porting the Jade Brick and Block

---
+++ src/main/java/codyhuh/druids_n_dinosaurs/Druids_n_dinosaurs.java
@@ -0,0 +1,28 @@
+package codyhuh.druids_n_dinosaurs;
+
+import codyhuh.druids_n_dinosaurs.Item.ModItemGroups;
+import codyhuh.druids_n_dinosaurs.Item.ModItems;
+import codyhuh.druids_n_dinosaurs.block.ModBlocks;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Druids_n_dinosaurs implements ModInitializer {
+	public static final String MOD_ID = "druids_n_dinosaurs";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
---
+++ src/main/java/codyhuh/druids_n_dinosaurs/Druids_n_dinosaursDataGenerator.java
@@ -0,0 +1,11 @@
+package codyhuh.druids_n_dinosaurs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Druids_n_dinosaursDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
